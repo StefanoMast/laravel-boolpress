30bb52c62dcad9e82328f9dbcc7d5333885eb790
StefanoMast <ste.mastrantonio@gmail.com>
2022-07-05 22:41:53 +0200

    add route change on admin page in providers->RouteServiceProvider.php

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -8,6 +8,6 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     public function index() {
-        dd ('Admin Home Controller');
+        return view('admin.home');
     }
 }
---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+<h1>ciao pagina amministratore</h1>
+    
+@endsection
