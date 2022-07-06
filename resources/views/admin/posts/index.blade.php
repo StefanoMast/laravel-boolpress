b1209a6f1440f12239a29c287ce7880e0949e9b0
StefanoMast <ste.mastrantonio@gmail.com>
2022-07-07 01:33:51 +0200

    add index posts (from admin folder)

---
+++ resources/views/admin/posts/index.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.dashboard')
+
+@section('content')
+    <h1>Lista dei tuoi post</h1>
+
+    <div class="row row-cols-3">
+        @foreach ($posts as $post)
+            {{-- single post --}}
+        <div class="col">
+            <div class="card mb-3" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">{{ $post->title}}</h5>
+                    <p class="card-text">Some quick example text to built on the card and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Leggi Post</a>
+                </div>
+            </div>
+        </div>
+        {{-- end single post --}}
+        @endforeach
+        
+    </div>
+@endsection
