d72368fcb15b85047d72b0e39dbd81809cb9cf11
StefanoMast <ste.mastrantonio@gmail.com>
2022-07-13 14:00:38 +0200

    created api with postman and by call in api.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,6 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(PostsTableSeeder::class);
         $this->call(CategoriesTableSeeder::class);
-        $this->call(TagSeeder::class);
+    //     $this->call(TagSeeder::class);
     }
 }
