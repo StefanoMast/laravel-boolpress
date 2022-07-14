45ca0e59a7df96bfcb2865872245b639091d0e7d
StefanoMast <ste.mastrantonio@gmail.com>
2022-07-14 22:18:03 +0200

    added error 404 with link to home

---
+++ resources/js/pages/NotFound.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <h2>Errore 404 pagina non trovata</h2>
+    <router-link :to="{name: 'home'}">Ritorna alla pagina Home</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "NotFound"
+}
+</script>
+
+<style>
+
+</style>
--- resources/js/router.js
+++ resources/js/router.js
@@ -6,6 +6,7 @@ Vue.use(VueRouter)
 import Home from "./pages/Home.vue";
 import About from "./pages/About.vue";
 import Blog from "./pages/Blog.vue";
+import NotFound from "./pages/NotFound.vue";
 const router = new VueRouter({
     mode: "history",
     routes: [
@@ -23,6 +24,10 @@ const router = new VueRouter({
             path: "/blog",
             name: "blog",
             component: Blog
+        },
+        {
+            path: "/*",
+            component: NotFound
         }
     ]
 });
